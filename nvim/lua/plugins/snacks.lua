7cb88b6bcf72b1bb9d3f677cf56514ec440c7ccc
khangtran2205 <procango2003@gmail.com>
2026-06-04 10:48:22 +0700

    make snacks.terminal floating

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -16,5 +16,13 @@ return {
         },
       },
     },
+    terminal = {
+      win = {
+        position = 'float',
+        width = 0.8,
+        height = 0.8,
+        border = 'rounded',
+      },
+    },
   },
 }
