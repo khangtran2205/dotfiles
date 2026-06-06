11c2786d218e4a73dc0df810ea0d123eef87471b
khangtran2205 <procango2003@gmail.com>
2026-06-06 22:55:26 +0700

    remove C-a keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,5 @@ local map = vim.keymap.set
 
 map('i', 'jj', '<Esc>', { noremap = false })
 
-map('n', '<C-a>', 'ggVG', { desc = 'Select all', noremap = false })
 map('n', 'ya', '<cmd>%y+<cr>', { desc = 'Select & yank all' })
 map('n', 'qq', '<cmd>close<cr>', { desc = 'Close current window', silent = true })
