bbd1014ad2ebf79595b866db5e0950132514834b
khangtran2205 <procango2003@gmail.com>
2026-06-10 10:30:51 +0700

    remove old config

--- nvim/lua/config/autocmds.lua
+++
@@ -1,5 +0,0 @@
--- Disable spell checker on markdown files
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
-  callback = function() vim.opt_local.spell = false end,
-})
--- nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
-local map = vim.keymap.set
-
-map('i', 'jj', '<Esc>', { noremap = false })
-
-map('n', 'ya', '<cmd>%y+<cr>', { desc = 'Select & yank all' })
-map('n', 'qq', '<cmd>close<cr>', { desc = 'Close current window', silent = true })
