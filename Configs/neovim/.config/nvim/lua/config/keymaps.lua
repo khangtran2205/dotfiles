d5f3ccb0fda4a667ccdf34dd31b152470206e03b
khangtran2205 <procango2003@gmail.com>
2026-06-14 22:56:20 +0700

    feat(nvim): add some keymaps

--- Configs/neovim/.config/nvim/lua/config/keymaps.lua
+++ Configs/neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,6 @@
+local map = vim.keymap.set
 
+map("n", "<C-a>", "ggVG", { desc = "Select all", noremap = false })
+map("n", "ya", "<cmd>%y+<cr>", { desc = "Select & yank all", silent = true })
+map("n", "qq", "<cmd>close<cr>", { desc = "Close current window", silent = true })
+map("i", "jj", "<Esc>", { noremap = false })
